95854a41b535ed24012f5c62d91b783e5ab05b56
gonzalo <equipostocker@gmail.com>
2020-05-02 16:31:07 -0300

    codigo PHP

---
+++ envia_mails_form.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['asunto']) && !empty($_POST['mensaje']) 
+        && !empty($_POST['email'])) {
+
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $asunto = $_POST['asunto'];
+        $email = $_POST['email'];
+        $cuerpo = $nombre . ' ' . $apellido . "\r\n";
+        $cuerpo .= $email . "\r\n" ;
+        $cuerpo .= $_POST['mensaje'];
+        $destino = "gonzalofigueras@gmail.com";
+        $header = "From: " . $email . "\r\n";
+        $header.= "Reply-To: " . $email . "\r\n";
+        $header.= "X-Mailer: PHP/". phpversion();
+        mail($destino,$asunto,$cuerpo,$header);
+    }
+}
+?>
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <div class="row justify-content-center">
 
                     <div class="col-lg-6  ">
-                        <form>
+                        <form action="" method="post">
                             <div class="form-group">
                                 <input type="text" name="nombre" class="form-control" placeholder="Nombre" required>
                             </div>
@@ -47,6 +47,7 @@
 
         </footer>
 </body>
+
 <script type="text/javascript">
     function enviar() {
         alert("Gracias!, su correo se envio satisfactoriamente");
@@ -54,6 +55,9 @@
 
     }
 </script>
+<?php
+    include("envia_mails_form.php");
+?>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
