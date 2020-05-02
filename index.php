bbfbfb8df30dd94afdadb70764b80191ff5a8eea
gonzalo <equipostocker@gmail.com>
2020-05-02 17:06:23 -0300

    ultima correcion

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                             <div class="form-group">
                                 <textarea name="mensaje" class="form-control" rows="5" placeholder="Cuerpo del mensaje..."></textarea>
                             </div>
-                            <button type="submit" onclick="enviar()" name="enviar" class="btn btn-secondary btn-lg btn-block">Enviar Correo</button>
+                            <button type="submit" onclick="enviarCorreo()" name="enviar" class="btn btn-secondary btn-lg btn-block">Enviar Correo</button>
                         </form>
                     </div>
                 </div>
@@ -47,17 +47,16 @@
 
         </footer>
 </body>
-
+<?php
+    include("envia_mails_form.php");
+?>
 <script type="text/javascript">
-    function enviar() {
+    function enviarCorreo() {
         alert("Gracias!, su correo se envio satisfactoriamente");
 
 
     }
 </script>
-<?php
-    include("envia_mails_form.php");
-?>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
